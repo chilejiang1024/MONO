78c152fb605de185f9ce672962c6a3a984551070
Chile Jiang <chilejiang1024@gmail.com>
2017-01-02 18:04:49 +0800

    test commit.

---
+++ ProjectA/Mine/View/SignInWithUMView.h
@@ -0,0 +1,13 @@
+//
+//  SignInWithUMView.h
+//  ProjectA
+//
+//  Created by JiangChile on 16/3/17.
+//  Copyright © 2016年 JiangChile. All rights reserved.
+//
+
+#import "BaseView.h"
+
+@interface SignInWithUMView : BaseView
+
+@end
---
+++ ProjectA/Mine/View/SignInWithUMView.m
@@ -0,0 +1,21 @@
+//
+//  SignInWithUMView.m
+//  ProjectA
+//
+//  Created by JiangChile on 16/3/17.
+//  Copyright © 2016年 JiangChile. All rights reserved.
+//
+
+#import "SignInWithUMView.h"
+
+@implementation SignInWithUMView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
